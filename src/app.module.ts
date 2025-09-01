645aa1035c2548049fd22458b13ec4dc54911877
fernandoraatz <fernando.raatz@gmail.com>
2025-09-01 13:16:32 -0300

    feat(event): add event module to main application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from "@nestjs/common";
-import { AccountModule, ResetModule, BalanceModule } from "./modules";
+import {
+  AccountModule,
+  ResetModule,
+  BalanceModule,
+  EventModule,
+} from "./modules";
 
 @Module({
-  imports: [AccountModule, ResetModule, BalanceModule],
+  imports: [AccountModule, ResetModule, BalanceModule, EventModule],
 })
 export class AppModule {}
